e8dc6f065529e8b6b5293b627b3f7b490e7c0c17
David Borrego <dbgjerez@gmail.com>
2018-03-02 20:24:28 +0100

    init

---
+++ src/main/java/com/dbg/quizback/QuizBackApplication.java
@@ -0,0 +1,12 @@
+package com.dbg.quizback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizBackApplication.class, args);
+	}
+}
