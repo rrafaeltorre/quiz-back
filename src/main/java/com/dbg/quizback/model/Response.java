0790cf50be8d6221ed7f63e0bda510968e66e675
David Borrego <dbgjerez@gmail.com>
2018-03-02 20:46:27 +0100

    respuestas, enunciados y tags

---
+++ src/main/java/com/dbg/quizback/model/Response.java
@@ -0,0 +1,13 @@
+package com.dbg.quizback.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Response {
+
+	String text;
+	Boolean correct;
+
+}
---
+++ src/main/java/com/dbg/quizback/model/Tag.java
@@ -0,0 +1,12 @@
+package com.dbg.quizback.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Tag {
+
+	String title;
+
+}
