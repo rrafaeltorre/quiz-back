f6dfa1cb0c875f8ee3e29c6feed74acc4b0614b0
David Borrego <dborrego@atsistemas.com>
2018-05-15 20:14:44 +0200

    mapeo de usuario

---
+++ src/main/java/com/dbg/quizback/model/User.java
@@ -0,0 +1,35 @@
+package com.dbg.quizback.model;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity
+public class User {
+
+	@Id
+	@GeneratedValue
+	private Integer idUser;
+
+	@Column(nullable = false)
+	private String name;
+
+	@Column(unique = true, nullable = false)
+	private String email;
+
+	private String password;
+
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = Result.FIELD_USER)
+	private List<Result> result;
+
+}
