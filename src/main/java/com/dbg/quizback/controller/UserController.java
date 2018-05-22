008902e9e91abc6586aaf1f80173a5b2e7cfd36b
David Borrego <dborrego@atsistemas.com>
2018-05-22 19:08:19 +0200

    añadida búsqueda de usuarios

--- src/main/java/com/dbg/quizback/controller/UserController.java
+++ src/main/java/com/dbg/quizback/controller/UserController.java
@@ -1,14 +1,35 @@
 package com.dbg.quizback.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import java.util.Set;
+import java.util.stream.Collectors;
 
+import org.dozer.DozerBeanMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dbg.quizback.dto.UserDTO;
+import com.dbg.quizback.model.User;
 import com.dbg.quizback.service.UserService;
 
-@Controller
+@RestController
+@RequestMapping(value = "/user")
 public class UserController {
 
 	@Autowired
 	UserService userService;
+
+	@Autowired
+	DozerBeanMapper dozer;
+
+	@RequestMapping(method = RequestMethod.GET)
+	public Set<UserDTO> findAll(@RequestParam(defaultValue = "0", required = false) Integer page,
+			@RequestParam(defaultValue = "10", required = false) Integer size) {
+		final Set<User> users = userService.findAll(PageRequest.of(page, size));
+		return users.stream().map(u -> dozer.map(u, UserDTO.class)).collect(Collectors.toSet());
+	}
 
 }
